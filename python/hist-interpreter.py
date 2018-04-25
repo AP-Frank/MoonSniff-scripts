82b762be00aa7726495cf2d08071396984d60771
Alexander Frank <ap.frank@tum.de>
2018-04-25 14:39:25 +0200

    prepare hist-interpreter to give more useful information

--- python/hist-interpreter.py
+++ python/hist-interpreter.py
@@ -1,5 +1,37 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import collections
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--tex', help="Use TeX for typesetting", action='store_true')
+args = parser.parse_args()
+
+########################
+# computed information #
+########################
+info_avg = list()
+info_stdDev = list()
+info_max = list()
+info_min = list()
+info_percentile = list()
+info_median = list()
+########################
+
+
+def get_percentile(percentile):
+    # compute percentiles
+    if percentile > 100 or percentile < 0:
+        print('cannot compute percentiles')
+        exit(-1)
+
+    targetSamples = samples * percentile / 100
+    sumsamples = 0
+
+    for k, v in histo.items():
+        sumsamples = sumsamples + v
+        if sumsamples > targetSamples:
+            return k
 
 bins = list()
 
@@ -7,31 +39,49 @@ for rate in range(1000, 10000, 1000):
     print(rate)
     averages = list()
 
-    for experiment in range(1, 5, 1):
-        file = open("rate-" + str(rate) + "-run-" + str(experiment) + ".csv")
+    file = open("rate-" + str(rate) + "-run-" + str(1) + ".csv")
 
-        samples = 0
-        sum = 0
+    # make sure the order within the dict is not changed
+    histo = collections.OrderedDict()
 
-        for line in file:
-            vals = line.split(",")
-            number = int(vals[0])
-            amount = int(vals[1])
+    samples = 0
+    sum = 0
 
-            sum += number * amount
-            samples += amount
+    for line in file:
+        vals = line.split(",")
+        number = int(vals[0])
+        amount = int(vals[1])
 
-        averages.append(sum / samples)
+        sum += number * amount
+        samples += amount
 
-    average = np.mean(averages)
-    print(averages)
+        histo[number] = amount
+
+    average = (sum / samples)
+
     print(average)
     bins.append(average)
+    print(histo)
+
+    # compute standard deviation
+    stdDevSum = 0
+    for k, v in histo.items():
+        stdDevSum = stdDevSum + v * ((k - average) ** 2)
+
+    info_avg.append(average)
+    info_stdDev.append((stdDevSum / (samples - 1)) ** 0.5)
+    info_min.append(next(iter(histo)))
+    info_max.append(next(iter(reversed(histo))))
+
+    # the .5 percentile is the median
+    info_median.append(get_percentile(50))
 
 rate = np.arange(1000, 10000, 1000)
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
+if args.tex:
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+
 plt.plot(rate, bins, rate, np.full((9,), 5))
 
 plt.xlabel('Average data-rate in [Mbit/s]')
