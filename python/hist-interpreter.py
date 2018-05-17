cc0dfcbea03e4e405f86927389888b3eadacc7ee
Alexander Frank <ap.frank@tum.de>
2018-05-17 20:07:58 +0200

    remove legacy code from main script

--- python/hist-interpreter.py
+++ python/hist-interpreter.py
@@ -4,8 +4,6 @@ import timeit
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.lines as mlines
-import collections
 
 start = timeit.default_timer()
 
@@ -16,34 +14,6 @@ parser.add_argument('-s', '--stepsize', help="The size ot the steps")
 parser.add_argument('-n', '--name', help="The common part of all file names (without <rate>.csv")
 parser.add_argument('-t', '--tex', help="Use TeX for typesetting", action='store_true')
 args = parser.parse_args()
-
-########################
-# computed information #
-########################
-info_avg = list()
-info_stdDev = list()
-info_max = list()
-info_min = list()
-info_percentile = list()
-info_median = list()
-
-
-########################
-
-
-def get_percentile(percentile):
-    # compute percentiles
-    if percentile > 100 or percentile < 0:
-        print('cannot compute percentiles')
-        exit(-1)
-
-    targetSamples = samples * percentile / 100
-    sumsamples = 0
-
-    for k, v in histo.items():
-        sumsamples = sumsamples + v
-        if sumsamples > targetSamples:
-            return k
 
 
 def ask_yes_no(question):
@@ -76,24 +46,6 @@ def save_figure(plt, filename):
 
     plt.savefig(directory + '/{}{:d}.png'.format(filename, ctr), dpi=200)
     print('Done saving')
-
-
-def plot_graph():
-    baseline_val = 5
-    baseline = np.full((len(bins),), baseline_val)
-
-    plt.plot(rate, bins, rate, baseline)
-
-    plt.xlabel('Average data-rate in [Mbit/s]')
-    plt.ylabel('Average latency in [ns]')
-    ax = plt.gca()
-    # ax.xaxis.set_major_locator(plt.MaxNLocator(2))
-    ax.xaxis.set_major_locator(plt.MultipleLocator(stepsize))
-    plt.title('Latency measurement for 1m optical fiber cable')
-    plt.legend(['measured', 'expected'])
-    plt.grid(True)
-    save_figure(plt, 'figure')
-    plt.show()
 
 
 def compute_ccdf(compressed_hist, index):
@@ -285,7 +237,8 @@ def read_samples():
             amount_lst.append(amount)
 
         compressed_hist.append((value_lst, amount_lst))
-        # new stuff
+
+        # TODO: instead of building a full sized list and reducing it later, build reduced one right away
         hist = np.repeat(value_lst, amount_lst)
 
         nth = max(1, int(sum(amount_lst) / 1000000))
@@ -341,75 +294,16 @@ def read_samples():
     return hists, points, compressed_hist
 
 
-# bins = list()
-#
-# hists = list()
-#
-# stepsize = int(args.stepsize)
-# begin = int(args.begin)
-# end = int(args.end) + stepsize
-#
-# for rate in range(begin, end, stepsize):
-#     value_lst = list()
-#     amount_lst = list()
-#
-#     print(rate)
-#     averages = list()
-#
-#     file = open("rate-" + str(rate) + "-run-" + str(1) + ".csv")
-#
-#     # make sure the order within the dict is not changed
-#     histo = collections.OrderedDict()
-#
-#     samples = 0
-#     sum = 0
-#
-#     for line in file:
-#         vals = line.split(",")
-#         number = int(vals[0])
-#         amount = int(vals[1])
-#         value_lst.append(number)
-#         amount_lst.append(amount)
-#
-#         sum += number * amount
-#         samples += amount
-#
-#         histo[number] = amount
-#
-#     average = (sum / samples)
-#
-#     # print(average)
-#     bins.append(average)
-#     print(histo)
-#
-#     # compute standard deviation
-#     stdDevSum = 0
-#     for k, v in histo.items():
-#         stdDevSum = stdDevSum + v * ((k - average) ** 2)
-#
-#     info_avg.append(average)
-#     info_stdDev.append((stdDevSum / (samples - 1)) ** 0.5)
-#     info_min.append(next(iter(histo)))
-#     info_max.append(next(iter(reversed(histo))))
-#
-#     # the .5 percentile is the median
-#     info_median.append(get_percentile(50))
-#
-#     # new stuff
-#     hist = np.repeat(value_lst, amount_lst)
-#     print(hist)
-#     hists.append(hist)
-#     print(hists)
-#
-# rate = np.arange(begin, end, stepsize)
-
-# print(bins)
-# print(rate)
-
 configure_plt_font()
 hists, points, compressed_hist = read_samples()
 
-# plot_graph()
+###################################
+# Uncomment the image to create   #
+#                                 #
+# Plotting more than one at a time#
+# is not tested yet               #
+###################################
+
 # violin_graph(hists, points, 1)
 box_graph(hists, points, 1)
 # plot_cdf_df(compressed_hist, points, 1)
