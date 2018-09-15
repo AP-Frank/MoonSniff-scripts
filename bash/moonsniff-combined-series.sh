3ba39a7d60e7533f2c62728ca8176b5564cfa9c0
Alexander Frank <ap.frank@tum.de>
2018-09-15 11:20:45 +0200

    fix typo

--- bash/moonsniff-combined-series.sh
+++ bash/moonsniff-combined-series.sh
@@ -7,7 +7,7 @@ cp configuration.sh logfiles/config
 
 for ((SEND_RATE=MS_START; SEND_RATE<=MS_END; SEND_RATE=SEND_RATE+MS_STEP));
 do
-	echo "Starting measurement for rate $SEND_RATE mbit\\s for $MS_TIME seconds .. "
+	echo "Starting measurement for rate $SEND_RATE Mbit/s for $MS_TIME seconds .. "
 	echo ""
 	./moonsniff-test.sh --rate $SEND_RATE --file ${SNIFFER_OUT_FILE}-rate-$SEND_RATE
 
